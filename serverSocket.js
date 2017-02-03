6e50f2f362cf3fc159e745e79c6079c8182a7aa9
Sierra <gsierra@guillesierra.com>
2017-02-03 20:50:22 -0300

    v1 - Working Backend - Added Clarfiai and Google, tested with android and clientSocket.js

--- serverSocket.js
+++ serverSocket.js
@@ -1,51 +1,24 @@
-// var Promise = require('bluebird');
-// var rpc = require('socket.io-rpc');
-
 /**Todo Change to Restify*/
 var app = require('express')();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3001;
 server.listen(port, function(){
     console.log('Server listening at port %d', port);
 });
 
+'use strict';
+
+
+
 /**Todo afterwards in order to implement a frontend
-app.get('/', function(req, res){
+ app.get('/', function(req, res){
     res.send('<h1>Hello world</h1>');
 });
-// Routing
-app.use(express.static(__dirname + '/public'));*/
+ // Routing
+ app.use(express.static(__dirname + '/public'));*/
 
-/*Instance of CV APIs
- * TODO add (and fix) Blippar*/
 
-/**Clarifai*/
-var Clarifai = require('clarifai');
-// initialize with your clientId and clientSecret
-var clarifai = new Clarifai.App(
-    'SieJMnA5BP4CkpL0YoXEGOEj7VKAGrH8VLZpD7zm',
-    'QQLo9NTDvhg9R32nQaC8Fb-ogAZDyzD4YPushXH6'
-  );
-
-function reqTagClarifai(data) {
-
-}
-
-/**Clodusight*/
-
-/**Google Cloud Vision API*/
-'use strict';
-// Imports the Google Cloud client library
-const Vision = require('@google-cloud/vision');
-// Your Google Cloud Platform project ID
-const projectId = '1074207413557';
-// Instantiates a client
-const visionClient = Vision({
-    projectId: projectId
-});
-
-const vision = Vision();
 
 //For Load-Balancers for general Multi-thread purposes
 var numUsers = 0;
@@ -60,47 +33,60 @@ io.on('connection', function (socket) {
         // socket.username = username;
         ++numUsers;
         addedUser = true;
-        console.log("User emitting: ");//+data);
+        console.log("User emitting: "+data[60]);
 
         /**Clarifai req*/
-        console.log("Sending data to Clarify: ");//+data);
-        clarifai.models.predict(Clarifai.GENERAL_MODEL, {base64: data}).then(
-            function(response) {
-                console.log("Clarifai response: "+response);
-                socket.emit('res', response);
-            },
-            function(err) {
-                console.error(err);
-            }
-        );
+        console.log("Sending data to Clarify: "+data[40]);
+        clarifai
+            .models
+            .predict(Clarifai.GENERAL_MODEL, {base64: data})
+            .then(
+                function(resp) {
+                    if (resp.status.description === 'Ok') {
+                        var concepts = resp.rawData.outputs[0].data.concepts;
+                        /**Todo get array?
+                             var data = resp.rawData.outputs[0].data.concepts;
+                             for(var i=0;i<=data.length;i++){
+                            // var tags = data[i].name;//collectTags(data);
+                            // tags = results[0].result.tag.classes;
+                         }*/
+                        console.log(concepts);
+                        socket.emit('EVENT_MESSAGE', concepts);
+                    } else {
+                        console.log('Sorry, something is wrong.\n'+resp.status.description);
+                    }            },
+                function(err) {
+                    console.log(err)
+                });
+        // socket.emit('EVENT_MESSAGE', predictClarifai(data));
 
         console.log("Would be now asking to Google: ");
 
         /**GoogleCloud req
-        console.log("Sending data to GoogleCloud: ");//+data);
-        vision.detectLabels(data).then(
-            function(results) {
+         console.log("Sending data to GoogleCloud: ");//+data);
+         vision.detectLabels(data).then(
+         function(results) {
                 const labels = results[0];
                 console.log("Google response: "+results);
                 // labels.forEach((label) => console.log(label));
                 socket.emit('res', results);
             },
-            function(err) {
+         function(err) {
                 console.error(err);
             }
-        );*/
+         );*/
         console.log("Here would go Google's response ");
 
         /* we tell the client to execute 'new message'
-        socket.broadcast.emit('new picReq', {
-            username: socket.username,
-            message: data
-        });*/
+         socket.broadcast.emit('new picReq', {
+         username: socket.username,
+         message: data
+         });*/
     });
 
     /**Not really required anymore...
-    // when the client emits 'add user', this listens and executes
-    socket.on('add user', function (username) {
+     // when the client emits 'add user', this listens and executes
+     socket.on('add user', function (username) {
         if (addedUser) return;
 
         // we store the username in the socket session for this client
@@ -117,15 +103,15 @@ io.on('connection', function (socket) {
         });
     });
 
-    // when the client emits 'typing', we broadcast it to others
-    socket.on('typing', function () {
+     // when the client emits 'typing', we broadcast it to others
+     socket.on('typing', function () {
         socket.broadcast.emit('typing', {
             username: socket.username
         });
     });
 
-    // when the client emits 'stop typing', we broadcast it to others
-    socket.on('stop typing', function () {
+     // when the client emits 'stop typing', we broadcast it to others
+     socket.on('stop typing', function () {
         socket.broadcast.emit('stop typing', {
             username: socket.username
         });
@@ -146,3 +132,52 @@ io.on('connection', function (socket) {
         }
     });
 });
+
+
+/*Instance of CV APIs
+ * TODO add (and fix) Blippar*/
+
+/**Clarifai*/
+var Clarifai = require('clarifai');
+// initialize with your clientId and clientSecret
+var clarifai = new Clarifai.App(
+    'SieJMnA5BP4CkpL0YoXEGOEj7VKAGrH8VLZpD7zm',
+    'QQLo9NTDvhg9R32nQaC8Fb-ogAZDyzD4YPushXH6'
+);
+
+function predictClarifai(imgBase64) {
+    clarifai
+        .models
+        .predict(Clarifai.GENERAL_MODEL, {base64: imgBase64})
+        .then(
+            function(resp) {
+                if (resp.status.description === 'Ok') {
+                    return resp.rawData.outputs[0].data.concepts;
+                    /**Todo get array?
+                     var data = resp.rawData.outputs[0].data.concepts;
+                     for(var i=0;i<=data.length;i++){
+                        // var tags = data[i].name;//collectTags(data);
+                        // tags = results[0].result.tag.classes;
+                        console.log(tags);
+                    }*/
+                    socket.emit('EVENT_MESSAGE', response);
+                } else {
+                    console.log('Sorry, something is wrong.\n'+resp.status.description);
+                }            },
+            function(err) {
+                console.log(err)
+            });
+}
+
+/**Clodusight*/
+
+/**Google Cloud Vision API*/
+// Imports the Google Cloud client library
+const Vision = require('@google-cloud/vision');
+// Your Google Cloud Platform project ID
+const projectId = '1074207413557';
+// Instantiates a client
+const visionClient = Vision({
+    projectId: projectId
+});
+const vision = Vision();

030ed0dd577df52749d44ef55bd784b653a17700
gsierra@guillesierra.com <glonchas@gmail.com>
2017-02-07 13:54:16 -0300

    v1.4.1 - Clean code, added google  credentials

--- README.md
+++ README.md
@@ -9,6 +9,9 @@ npm install socket.io --save
 npm install clarifai --save
 npm install cloudsight --save
 npm install @google-cloud/vision --save
+npm install googleapis --save
+npm install google-auth-library --save
+
 ```
 (ToBe Deprecated)
 ```
--- serverSocket.js
+++ serverSocket.js
@@ -55,10 +55,42 @@ function predictClarifai(imgBase64) {
 // Imports the Google Cloud client library
 const GoogleVision = require('@google-cloud/vision');
 // Your Google Cloud Platform project ID
-const projectId = 'shotgot-156720';//'1074207413557';
+var projectId = 'shotgot-156720';//'1074207413557';
 // Instantiates a client
 const googleCloud = GoogleVision({
     projectId: projectId
+});
+
+// This method looks for the GCLOUD_PROJECT and GOOGLE_APPLICATION_CREDENTIALS
+// environment variables.
+google.auth.getApplicationDefault(function (err, authClient, projectId) {
+    if (err) {
+        throw err;
+    }
+
+    // The createScopedRequired method returns true when running on GAE or a local developer
+    // machine. In that case, the desired scopes must be passed in manually. When the code is
+    // running in GCE or a Managed VM, the scopes are pulled from the GCE metadata server.
+    // See https://cloud.google.com/compute/docs/authentication for more information.
+    if (authClient.createScopedRequired && authClient.createScopedRequired()) {
+        // Scopes can be specified either as an array or as a single, space-delimited string.
+        authClient = authClient.createScoped([
+            'https://www.googleapis.com/auth/compute'
+        ]);
+    }
+
+    // Fetch the list of GCE zones within a project.
+    // NOTE: You must fill in your valid project ID before running this sample!
+    var compute = google.compute({
+        version: 'v1',
+        auth: authClient
+    });
+    compute.zones.list({
+        project: projectId,
+        auth: authClient
+    }, function (err, result) {
+        console.log(err, result);
+    });
 });
 
 //For metrics, Load-Balancers (Multi-thread purposes)

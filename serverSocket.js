791e390130dcbc8ff202eec1985723aa6b017a71
Sierra <gsierra@guillesierra.com>
2017-02-07 20:42:54 -0300

    v2.1 -  Refactored and clean code - Working Cloudsight due to error in filesystem add

--- serverSocket.js
+++ serverSocket.js
@@ -52,98 +52,98 @@ io.on('connection', function (socket) {
         // console.log("filename created: "+ filename);
 
         fs.writeFile("img.jpg",  new Buffer(base64Data, "base64"), function(err) {
-            console.log("FileCreationError: "+ err);
-        });
+            if(err) console.log("FileCreationError: "+ err);
 
-        /**Google req*/
-        console.log("Sending data to Google: ");
-        googleVision.detectLogos(filename).then(
-            function(resp) {
-                const logos = resp[0];
-                // console.log('Logos:');
-                // logos.forEach((logo) => console.log(logo);
-                socket.emit('GOOGLE_LOGOS', logos);
-                console.log("GoogleLogo: "+ logos);
-            }, function(err) {
-                console.log("GoogleLogoError: "+ err);
-            });
-        googleVision.detectLabels(filename).then(
-            function(resp) {
-                const labels = resp[0];
-                socket.emit('GOOGLE_LABELS', labels);
-                console.log("GoogleLabel: "+labels);
-            }, function(err) {
-                console.log("GoogleLabelError: "+ err);
-            });
-        googleVision.detectText(filename).then(
-            function(resp) {
-                const text = resp[0];
-                socket.emit('GOOGLE_TEXT', text);
-                console.log("GoogleText: "+text);
-            }, function(err) {
-                console.log("GoogleTextError: "+ err);
-            });
-        /**Cloudsight req*/
-        console.log("Sending data to Cloudsight: "+base64Data[40]);
+            /**Google req*/
+            console.log("Sending data to Google: ");
+            googleVision.detectLogos(filename).then(
+                function(resp) {
+                    const logos = resp[0];
+                    // console.log('Logos:');
+                    // logos.forEach((logo) => console.log(logo);
+                    socket.emit('GOOGLE_LOGOS', logos);
+                    console.log("GoogleLogo: "+ logos);
+                }, function(err) {
+                    console.log("GoogleLogoError: "+ err);
+                });
+            googleVision.detectLabels(filename).then(
+                function(resp) {
+                    const labels = resp[0];
+                    socket.emit('GOOGLE_LABELS', labels);
+                    console.log("GoogleLabel: "+labels);
+                }, function(err) {
+                    console.log("GoogleLabelError: "+ err);
+                });
+            googleVision.detectText(filename).then(
+                function(resp) {
+                    const text = resp[0];
+                    socket.emit('GOOGLE_TEXT', text);
+                    console.log("GoogleText: "+text);
+                }, function(err) {
+                    console.log("GoogleTextError: "+ err);
+                });
+            /**Cloudsight req*/
+            console.log("Sending data to Cloudsight: "+base64Data[40]);
 
-        var imgCloudsight = {
-            image: '/img.jpg',
-            locale: 'en-US'  //Todo Add TTL ?
-        };
-        cloudsight.request (imgCloudsight, true, function(err, resp) {
-            if (err) {
-                console.log ("Cloudsight error: "+err);
-                return;
-            }
-            if (resp.status === 'completed') {
-                console.log(resp.name);
-                socket.emit('CLOUDSIGHT', resp.name);
-            } else {
-                console.log('Sorry, something is wrong.\n'+resp.status);
-            }
+            var imgCloudsight = {
+                image: '/img.jpg',
+                locale: 'en-US'  //Todo Add TTL ?
+            };
+            cloudsight.request (imgCloudsight, true, function(err, resp) {
+                if (err) {
+                    console.log ("Cloudsight error: "+err);
+                    return;
+                }
+                if (resp.status === 'completed') {
+                    console.log(resp.name);
+                    socket.emit('CLOUDSIGHT', resp.name);
+                } else {
+                    console.log('Sorry, something is wrong.\n'+resp.status);
+                }
+            });
         });
 
         /**Clarifai req*/
         console.log("Sending data to Clarify: "+base64Data[40]);
         clarifai.models.predict(Clarifai.GENERAL_MODEL, {base64: base64Data}).then(
-                function(resp) {
-                    //resp.getOuputInfo()
+            function(resp) {
+                //resp.getOuputInfo()
 
-                    //Blacklist as array.notincase()
-                    var clarifai_blacklist = [ "no person", "indoors", "one", "empty", "furniture"];
-                    if (resp.status.description === 'Ok') {
-                        //TODO ToBeChanged... FOREACH
-                        var concepts = resp.rawData.outputs[0].data.concepts;
-                        for(var i=0;i<=concepts.length;i++){
-                            if(concepts[i].value>0.7){
-                                //Foreach
-                                socket.emit('CLARIFAI_CONCEPTS', concepts[i].name);
-                                console.log("Clarifai: "+concepts[i].name);
-                            }
+                //Blacklist as array.notincase()
+                var clarifai_blacklist = [ "no person", "indoors", "one", "empty", "furniture"];
+                if (resp.status.description === 'Ok') {
+                    //TODO ToBeChanged... FOREACH
+                    var concepts = resp.rawData.outputs[0].data.concepts;
+                    for(var i=0;i<=concepts.length;i++){
+                        if(concepts[i].value>0.7){
+                            //Foreach
+                            socket.emit('CLARIFAI_CONCEPTS', concepts[i].name);
+                            console.log("Clarifai: "+concepts[i].name);
                         }
-                    } else {
-                        console.log('Sorry, something is wrong.\n'+resp.status.description);
-                    }            },
-                function(err) {
-                    console.log(err)
-                });
+                    }
+                } else {
+                    console.log('Sorry, something is wrong.\n'+resp.status.description);
+                }            },
+            function(err) {
+                console.log(err)
+            });
 
         console.log("Req color to Clarify: "+base64Data[40]);
         clarifai.models.predict(Clarifai.COLOR_MODEL, {base64: base64Data}).then(
-                function(resp) {
-                    if (resp.status.description === 'Ok') {
-                        var col = resp.rawData.outputs[0].data.colors;
-                        for(var i=0;i<=col.length;i++){
-                            // if(col[i].value>0.5)
-                            socket.emit('CLARIFAI_COLOR', col[i].w3c.hex);
-                            console.log(col[i].w3c.hex);
-                        }
-                    } else {
-                        console.log('Sorry, something is wrong.\n'+resp.status.description);
-                    }            },
-                function(err) {
-                    console.log(err)
-                });
+            function(resp) {
+                if (resp.status.description === 'Ok') {
+                    var col = resp.rawData.outputs[0].data.colors;
+                    for(var i=0;i<=col.length;i++){
+                        // if(col[i].value>0.5)
+                        socket.emit('CLARIFAI_COLOR', col[i].w3c.hex);
+                        console.log(col[i].w3c.hex);
+                    }
+                } else {
+                    console.log('Sorry, something is wrong.\n'+resp.status.description);
+                }            },
+            function(err) {
+                console.log(err)
+            });
     });
 
     /**TODO handle disconnections from client*/
